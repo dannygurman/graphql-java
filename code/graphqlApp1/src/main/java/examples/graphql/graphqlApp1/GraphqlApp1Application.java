257c86125a2970fa573340dad8de1468d47366dc
dannygurman <danny.gurman@gmail.com>
2024-08-28 19:39:39 +0300

    Init1

---
+++ code/graphqlApp1/src/main/java/examples/graphql/graphqlApp1/GraphqlApp1Application.java
@@ -0,0 +1,13 @@
+package examples.graphql.graphqlApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlApp1Application.class, args);
+	}
+
+}
