24cfb6f595308a2d537c1c3f3ed16d5fa4c4dc65
dannygurman <danny.gurman@gmail.com>
2024-08-29 21:27:26 +0300

    Init7

---
+++ code/graphqlApp2/src/main/java/examples/graphql/graphqlApp2/intro/Author.java
@@ -0,0 +1,12 @@
+package examples.graphql.graphqlApp2.intro;
+
+
+import lombok.Data;
+
+@Data
+public class Author {
+
+    private String id;
+    private String name;
+    private String thumbnail;
+}
